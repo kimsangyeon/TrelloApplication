09e239246d95e517592ddcb20ac4b074e446be31
Kim Sangyeon <dho113@naver.com>
2018-09-16 11:20:38 +0900

    task/task3 - homepage component service와 연결

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Board} from '../model/board';
+import {SubTask} from '../model/subtask';
+import {Task} from '../model/task';
+import {TrelloService} from '../services/trello.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  boards: Board[]= Array();
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private _trelloService:TrelloService,private _router: Router) { }
 
   ngOnInit() {
+    this.boards.push (this._trelloService.seedData());
   }
-
+  public addBoard(){
+    console.log('Adding new board');
+    let newBoard:Board= new Board();
+    newBoard.id = this.boards.length + 1;
+    newBoard.task = new Array();
+    newBoard.title = "New Board";
+    this.boards.push(newBoard);
+    console.log('new board added');
+  }
 }
